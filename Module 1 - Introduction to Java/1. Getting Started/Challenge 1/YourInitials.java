04d600bb54bf956f13baedcadc1045451871139c
Prince Addai <addap@amazon.com>
2022-12-28 16:29:47 -0500

    points

--- Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,12 @@
 public class YourInitials {
     public static void main(String[] args) {
          // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+        System.out.println("PPPP     A  ");
+        System.out.println("P   P   A A ");
+        System.out.println("P   P  A   A");
+        System.out.println("PPPP   AAAAA");
+        System.out.println("P      A   A");
+        System.out.println("P      A   A");
+        System.out.println("P      A   A");
     }
 }
