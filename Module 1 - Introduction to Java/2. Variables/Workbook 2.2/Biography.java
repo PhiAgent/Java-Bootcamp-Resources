9b2fe510950c35c5d90e6d30389a6c596c330eb8
Prince Addai <addap@amazon.com>
2022-12-28 16:56:04 -0500

    bio

--- Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
+++ Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
@@ -5,18 +5,26 @@ public class Biography {
 
 
         // name (text)
+        String name = "Prince Gyekye Addai";
         // age (whole number)
+        int age = 29;
         // country (text)
+        String country = "Ghana";
         // sport (text)
+        String sport = "table tennis";
         // hours (whole number)
+        int hours = 3;
         // game (text)
+        String game = "Horizon";
         // subject (text)
+        String subject = "Physics";
         // grade (character)
+        char grade = 'B';
      
-        System.out.println("My name is <name>. I'm <age> years old, and I'm from <country> ");
-        System.out.println("My favourite sport is <sport>. I play for <hours> hours a day");
-        System.out.println("When I'm tired, I like to play <game>.");
-        System.out.println("In school, my favourite subject was <subject>, I scored a <grade>.");
+        System.out.println("My name is "+name+". I'm "+age+" years old, and I'm from "+country+" ");
+        System.out.println("My favourite sport is "+sport+". I play for "+hours+" hours a day");
+        System.out.println("When I'm tired, I like to play "+game+".");
+        System.out.println("In school, my favourite subject was "+subject+", I scored a "+grade+".");
 
         // Compare your result to what's on Learn the Part.
     }
